14505c16a62f39085da23b870ac0ad4ec55b76e1
allisson79 <thomas@allisson.no>
2025-05-01 11:08:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,19 +3,22 @@ def troll_tove():
     spadom = ""
     intro_valg = ""
     sporsmal = ""
-    
-    if request.method == "POST":
+
+    if    request.method == "POST":
         sporsmal = request.form["sporsmal"]
         intro_valg = random.choice(intro)
         spm = sporsmal.lower()
 
-    if "kamp" in spm or "neste kamp" in spm or "lillestrøm" in spm:
+        if    "kamp" in spm or "neste kamp" in spm or "lillestrøm" in spm:
             spadom = neste_glimt_kamp()
 
-        elif any(word in spm for word in ["glimt", "bodø", "fotball", "eliteserien", "rosenborg", "molde", "tromsø", "til", "rbk"]):
+        elif    any(word in spm for word in [
+            "glimt", "bodø", "fotball", "eliteserien", "rosenborg",
+            "molde", "tromsø", "til", "rbk"
+        ]):
             spadom = random.choice(spaadommer_fotball)
 
-        else:
+        else:    
             spadom = random.choice(spaadommer_random)
 
     return render_template("index.html", spadom=spadom, intro=intro_valg, sporsmal=sporsmal)
